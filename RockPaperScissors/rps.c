7574b6532f3f47faf12f62c574db595ee31fd09a
Karthik Rajan <karthikrcms@gmail.com>
2026-03-15 17:49:38 -0400

    add rpsResult function to pass first test

--- RockPaperScissors/rps.c
+++ RockPaperScissors/rps.c
@@ -1,4 +1,9 @@
+#include <string.h>
+
 char* rpsResult(char* player1, char* player2)
 {
+    if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
+        return "Player1";
+
     return "";
 }
